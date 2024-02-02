ed28e5fe3508e81f8d8a2018fdf11780e3cdef76
Dalton Tewanger <daltontewanger@yahoo.com>
2024-02-02 09:35:38 -0800

    Added stock api for data and created a mock chart using chart.js

--- public/index.js
+++ public/index.js
@@ -1,9 +1,28 @@
 async function main() {
 
+    let response = await fetch('https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1min&apikey=bb65f132b1654f74863982595aa828a4');
+    let result = await response.json();
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+    let {GME, MSFT, DIS, BNTX} = result
+    let stocks = [GME, MSFT, DIS, BNTX];
+    console.log(result)
+
+    new Chart(timeChartCanvas.getContext('2d'), {
+        type: 'line',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor:  'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgba(255, 99, 132, 1)'
+            }]
+        }
+    });
+    
 }
 
 main()
